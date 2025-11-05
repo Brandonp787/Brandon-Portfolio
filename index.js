07fc1499ead5206a8ac08da13631471d37b4f45a
Brandon <brandonp919@gmail.com>
2025-11-05 00:11:42 -0500

    Implement sendMail function for email sending
    Added a function to send email using EmailJS.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 window.alert("welcome to my site!"); 
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").Value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_2omki7c","template_dlu2x6n",parms).then(alert("Email sent!!"))
+}
